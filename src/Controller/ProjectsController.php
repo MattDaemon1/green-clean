424b1fe787a90de8d9923ac9d6066c2a2f4e0205
MattDaemon1 <matt@mattkonnect.com>
2024-05-10 19:02:39 +0200

    Afficha du formulaire et formulaire fonctionnel

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Donations;
 use App\Entity\Projects;
+use App\Form\DonationsType;
 use App\Repository\ProjectsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,11 +27,22 @@ class ProjectsController extends AbstractController
     }
 
     #[Route('/projects/{id}', name: 'app_projects_show')]
-    public function show(Projects $projects): Response
+    public function show(Projects $projects, Request $request, EntityManagerInterface $entityManager): Response
     {
-        dump($projects);
+        $donations = new Donations();
+        $form = $this->createForm(DonationsType::class, $donations);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($donations);
+            $entityManager->flush();
+            
+
+        }
+
         return $this->render('projects/show.html.twig', [
             'projets' => $projects,
+            'form' => $form,
         ]);
     }
 }
