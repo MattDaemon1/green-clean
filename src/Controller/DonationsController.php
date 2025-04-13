88affffdea00fb89f770e5b6f30228d3de464698
MattDaemon1 <matt@mattkonnect.com>
2025-04-13 23:24:27 +0200

    Implement CRUD functionality for donations in admin panel

--- src/Controller/DonationsController.php
+++ src/Controller/DonationsController.php
@@ -19,4 +19,39 @@ class DonationsController extends AbstractController
             'donations' => $donations,
         ]);
     }
+
+    #[Route('/admin/donations/create', name: 'admin_donations_create')]
+    public function create(EntityManagerInterface $entityManager): Response
+    {
+        // Logic for creating a new donation (form handling, etc.)
+        return $this->render('admin/donations_form.html.twig');
+    }
+
+    #[Route('/admin/donations/edit/{id}', name: 'admin_donations_edit')]
+    public function edit(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $donation = $entityManager->getRepository(Donations::class)->find($id);
+
+        if (!$donation) {
+            throw $this->createNotFoundException('Donation not found');
+        }
+
+        // Logic for editing the donation (form handling, etc.)
+        return $this->render('admin/donations_form.html.twig', [
+            'donation' => $donation,
+        ]);
+    }
+
+    #[Route('/admin/donations/delete/{id}', name: 'admin_donations_delete')]
+    public function delete(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $donation = $entityManager->getRepository(Donations::class)->find($id);
+
+        if ($donation) {
+            $entityManager->remove($donation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_donations');
+    }
 }
