442a00143355c949340d0b27dbae549c902def88
MattDaemon1 <matt@mattkonnect.com>
2025-06-22 14:47:10 +0200

    Erreur 404 résolu

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -7,15 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProjectsRepository $projectsRepository, ParameterBagInterface $parameterBagInterface): Response
+    public function index(EntityManagerInterface $entityManager, ParameterBagInterface $parameterBagInterface): Response
     {
-
         $limit = $parameterBagInterface->get('home_projets_limit');
-        $projets = $projectsRepository->findBy([], ['id' => 'DESC'],$limit);
+        $repository = $entityManager->getRepository(\App\Entity\Projects::class);
+        $projets = $repository->findBy([], ['id' => 'DESC'], $limit);
 
         $websiteName = 'Green & Clean';
         return $this->render('page/index.html.twig', [
