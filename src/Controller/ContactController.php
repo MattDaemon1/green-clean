3f7171ac7eb6498cf5b41fa03045355e0aa38e03
MattDaemon1 <matt@mattkonnect.com>
2025-07-04 18:09:26 +0200

    Pré deploy

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,13 +7,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function contact(Request $request, MailerInterface $mailer): Response
+    public function contact(Request $request): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
@@ -21,21 +19,14 @@ class ContactController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            
-            // Création et envoi de l'email
-            $email = (new Email())
-                ->from($data['email']) // Utilise l'email saisi dans le formulaire
-                ->to('matt@mattkonnect.com') // Votre adresse de réception
-                ->subject('Nouveau message de contact: ' . $data['subject'])
-                ->text('Expéditeur: ' . $data['name'] . ' (' . $data['email'] . ")\n\n" . $data['message']);
+            // Par exemple, envoyer un email :
+            // $mailer->send(...);
 
-            try {
-                $mailer->send($email);
-                $success = true;
-            } catch (\Exception $e) {
-                // Vous pourriez logger l'erreur ou afficher un message à l'utilisateur
-                $this->addFlash('error', 'Une erreur est survenue lors de l\'envoi du message : ' . $e->getMessage());
-            }
+            // Ou enregistrer le message en base de données :
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($data);
+            // $entityManager->flush();
+            $success = true;
         }
 
         return $this->render('page/contact.html.twig', [
